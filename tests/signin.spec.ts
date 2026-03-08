d9ab05b28e507c3f076c8bca4422285577998e36
Kateryna <ekaterinamkg@gmail.com>
2026-03-08 19:44:10 +0100

    first-commit-to-main

---
+++ tests/signin.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "@playwright/test";
+import { beforeEach } from "node:test";
+
+test.describe("Sign up test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    await page.locator(".header_signin").click();
+  });
+  test("Successful sign up", async ({ page }) => {
+    await page.goto("/");
+    await page.locator("h1").highlight();
+  });
+
+  //   test("Filter", asynk({ page }) => {
+  //     await page.locator('button').filter({hasText: 'contacts'}).highlight()
+  //   });
+});
